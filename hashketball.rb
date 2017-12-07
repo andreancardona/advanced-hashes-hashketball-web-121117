13494803df90b747397688a2acf5d7a9856a9997
andreancardona@gmail.com <andreancardona@gmail.com>
2017-12-07 17:31:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,3 +115,14 @@ def game_hash
    }
  }
  end
+
+ def num_points_scored(player_name)
+ 
+   game_hash.values.each do |team|  
+    team[:players].each do |player|
+       return player[:points] if player.has_value?(player_name)
+     end
+   end
+ 
+ end
+
