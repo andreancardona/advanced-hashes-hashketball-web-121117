7d74b1685bbb9b19c5b8c780565f54cd35243adb
andreancardona@gmail.com <andreancardona@gmail.com>
2017-12-07 18:38:07 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def player_numbers(team) #accepts an argument and #expects an a new array of num
    end
 
 
-   def big_shoe_rebounds #does NOT accept an argument 
+   def big_shoe_rebounds #does NOT accept an argument
     rebound = 0
     shoe_size = 0
     game_hash.each do |team_side, team_details|
